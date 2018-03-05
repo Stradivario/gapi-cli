44fdb5c9f087ef84676cdf00522c011582170615
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2018-03-05 10:50:40 +0200

    chore(README.md): added basic documentation

---
+++ README.md
@@ -0,0 +1,17 @@
+## This is CLI project related with GAPI graphQL api with typescript Decorators
+
+#### To install gapi-cli utility it type following command:
+
+```bash
+npm i -g gapi-cli
+```
+
+#### To create new project from scratch via CLI type:
+
+```bash
+gapi-cli new my-project
+```
+
+#### [GAPI](https://github.com/Stradivario/gapi)
+
+#### [Gapi project starter](https://github.com/Stradivario/gapi-starter)
--- tasks/new.js
+++ tasks/new.js
@@ -2,10 +2,4 @@
 var shell = require("shelljs");
 
 shell.exec(
-    `git clone https://github.com/Stradivario/gapi-starter.git ${process.argv[2]} && cd ./${process.argv[2]} && npm install`,
-    function(code, stdout, stderr) {
-        console.log('Exit code:', code);
-        console.log('Program output:', stdout);
-        console.log('Program stderr:', stderr);
-      }
-);
+    `git clone https://github.com/Stradivario/gapi-starter.git ${process.argv[2]} && cd ./${process.argv[2]} && npm install`);
