42826b52f8499cf1814a0dc9cb3b0eddd30b46ea
Kristiyan Tachev <kristiqn.tachev@gmail.com>
2019-05-02 02:53:09 +0300

    feature(Introspection): added introspection reoved from gitignore

---
+++ dist/daemon-server/api-introspection/index.d.ts
@@ -0,0 +1,38 @@
+export interface IGraphQLResponseRoot {
+    data?: IQuery | IMutation;
+    errors?: Array<IGraphQLResponseError>;
+}
+export interface IGraphQLResponseError {
+    message: string;
+    locations?: Array<IGraphQLResponseErrorLocation>;
+    [propName: string]: any;
+}
+export interface IGraphQLResponseErrorLocation {
+    line: number;
+    column: number;
+}
+/**
+  description: Query type for all get requests which will not change persistent data
+*/
+export interface IQuery {
+    __typename?: "Query";
+    status: IStatusQueryType | null;
+    getLinkList: Array<ILinkListType> | null;
+}
+export interface IStatusQueryType {
+    __typename?: "StatusQueryType";
+    status: string | null;
+}
+export interface ILinkListType {
+    __typename?: "LinkListType";
+    repoPath: string | null;
+    introspectionPath: string | null;
+    linkName: string | null;
+}
+/**
+  description: Mutation type for all requests which will change persistent data
+*/
+export interface IMutation {
+    __typename?: "Mutation";
+    notifyDaemon: ILinkListType | null;
+}
